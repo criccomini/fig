f4c8665ea97216c1ad3ae1ab9c01adabdb66b39f
Chris Riccomini <criccomi@criccomi-ld.linkedin.biz>
2012-03-23 15:07:21 -0700

    fixing config exception

--- fig-core/src/main/java/fig/ConfigException.java
+++ fig-core/src/main/java/fig/ConfigException.java
@@ -2,19 +2,16 @@ package fig;
 
 public class ConfigException extends RuntimeException {
   private static final long serialVersionUID = 1L;
-  private final Throwable wrappedException;
-  private final String msg;
 
   public ConfigException(Throwable e) {
-    this(null, e);
+    super(e);
   }
 
   public ConfigException(String msg) {
-    this(msg, null);
+    super(msg);
   }
 
   public ConfigException(String msg, Throwable e) {
-    this.msg = msg;
-    this.wrappedException = e;
+    super(msg, e);
   }
 }
